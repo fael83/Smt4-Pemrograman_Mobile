f918c0caa4f946c3d722d30b090ac1f1bc0213a8
fael83 <rafaellokajaya@gmail.com>
2026-04-13 22:54:44 +0700

    Percobaan 2

--- basic_layout_flutter/README.md
+++ basic_layout_flutter/README.md
@@ -2,3 +2,6 @@
 
 1
 ![Screenshoot basic_layout_flutter](images/01.png)
+
+2
+![Screenshoot basic_layout_flutter](images/02.png)
--- basic_layout_flutter/lib/main.dart
+++ basic_layout_flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
+  debugPaintSizeEnabled = true; // Remove to suppress visual layout
   runApp(const MyApp());
 }
 
@@ -9,15 +11,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.white),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 32, color: Colors.black87),
-        ),
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        // Change to buildColumn() for the other column example
+        body: Center(child: buildRow()),
       ),
     );
   }
+
+  Widget buildRow() =>
+      // #docregion row
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      );
+  // #enddocregion row
+
+  Widget buildColumn() =>
+      // #docregion column
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      );
+  // #enddocregion column
 }
